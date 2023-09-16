d702544d5fed8d5c8a20cb25af129701923b2438
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2023-09-16 21:21:50 +0530

    Initial Commit

---
+++ Fantasy Open/Fantasy_OpenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fantasy_OpenApp.swift
+//  Fantasy Open
+//
+//  Created by Sanjay Siddharth on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Fantasy_OpenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
